f363c4731cbf60a51d98b541166349dbe75125c9
OliverBryan <oliverbryan1203@gmail.com>
2020-10-28 19:31:21 +0000

    Added launch.json

--- Main.dart
+++ Main.dart
@@ -1,1 +1,11 @@
+void main() {
+  // You can modify this file to do any testing you wish
+  // Remember that your changes will not be saved
 
+  // To run, type "dart Main.dart" in the console
+
+  // You can also go to the debug menu and click "Start Without Debugging"
+  // or use the keyboard shortcut Ctrl+F5
+
+  print("Dart is being run in gitpod");
+}
